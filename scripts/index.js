fd993d3ce3365b3fd04e35926df947c9fc45a33e
AnthonyByc <anthony.f.boyac@gmail.com>
2020-11-18 19:19:25 -0800

    Added observer for animations

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,43 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-  
-      if (this.hash !== "") {
-        event.preventDefault();
-  
-        var hash = this.hash;
-  
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      }
-    });
+$(document).ready(function () {
+
+  /* Smooth scroll function when clicking menu anchors */
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      // 800 specifies the number of milliseconds it takes to scroll to the specified area (optional)
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
   });
+
+  var sectionId1 = document.querySelector("#about");
+  var sectionId2 = document.querySelector("#get-in-touch");
+  var animationItem = "slide-in 2s forwards";
+
+  var observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting == true) {
+        if (entry.target.id == "about") {
+          document.querySelector(".scroll-down-container").style.animation = "scroll-hide 2.5s forwards";
+          document.querySelector("#about .flexbox-item:nth-of-type(1)").style.animation = animationItem;
+          document.querySelector("#about .flexbox-item:nth-of-type(2)").style.animation = animationItem;
+          observer.unobserve(sectionId1);
+        } else if (entry.target.id == "get-in-touch") {
+          document.querySelector("#get-in-touch .flexbox-item:nth-of-type(1)").style.animation = animationItem;
+          document.querySelector("#get-in-touch .flexbox-item:nth-of-type(2)").style.animation = animationItem;
+          observer.unobserve(sectionId2);
+        }
+      }
+    })
+  });
+  observer.observe(sectionId1);
+  observer.observe(sectionId2)
+});
